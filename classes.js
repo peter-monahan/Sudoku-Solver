9910e03b573956db867aa50ee5b8cb366a19bfa3
peter <peter.b.monahan@gmail.com>
2023-09-06 16:44:34 -0500

    added classes

--- classes.js
+++ classes.js
@@ -1,1 +1,32 @@
 // Solver classes
+
+class SudokuSolver {
+  constructor(sudoku) {
+
+    this.sudoMatrix = sudoku;
+    this.rows = [];
+    this.cols = [];
+    this.blocks = [[],[],[]];
+    this.squares = [[],[],[],[],[],[],[],[],[]];
+  }
+
+  solve() {
+
+  }
+}
+
+class SudokuRow {
+
+}
+
+class SudokuCol {
+
+}
+
+class SudokuBlock {
+
+}
+
+class SudokuSquare {
+
+}

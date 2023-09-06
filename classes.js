1a14c8e9e45fbd3e283a0db778e727fa6f9d6ef2
peter <peter.b.monahan@gmail.com>
2023-09-06 16:56:33 -0500

    Partially completed constructors

--- classes.js
+++ classes.js
@@ -8,6 +8,25 @@ class SudokuSolver {
     this.cols = [];
     this.blocks = [[],[],[]];
     this.squares = [[],[],[],[],[],[],[],[],[]];
+
+    for (let i = 0; i < this.sudoMatrix.length; i++) {
+      this.rows.push(new SudokuRow(this, i));
+      this.cols.push(new SudokuCol(this, i));
+    }
+
+    for (let i = 0; i < this.blocks.length; i++) {
+      const blockRow = this.blocks[i];
+      for (let j = 0; j < 3; j++) {
+        blockRow.push(new SudokuBlock(this, i, j));
+      }
+    }
+
+    for (let i = 0; i < this.squares.length; i++) {
+      const row = this.squares[i];
+      for (let j = 0; j < this.sudoMatrix.length; j++) {
+        row.push(new SudokuSquare(this, i, j));
+      }
+    }
   }
 
   solve() {
@@ -16,17 +35,25 @@ class SudokuSolver {
 }
 
 class SudokuRow {
+  constructor(solver, row) {
 
+  }
 }
 
 class SudokuCol {
+  constructor(solver, col) {
 
+  }
 }
 
 class SudokuBlock {
+  constructor(solver, row, col) {
 
+  }
 }
 
 class SudokuSquare {
+  constructor(solver, row, col) {
 
+  }
 }
